4d88f240c17ba296d7b3a943625b13c43635c809
Thomas Chan Lund Nielsen <thom8203@edu.eal.dk>
2019-05-28 13:32:54 +0200

    blablaba

---
+++ SEO projekt/Bolierplate/css/kort.js
@@ -0,0 +1,43 @@
+
+    document.getElementById("tre1").addEventListener("click", myFunction);
+       function myFunction() {
+        var x = document.getElementById("sideBar");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+
+
+    document.getElementById("krydset").addEventListener("click", myFunction);
+    function myFunction() {
+     var x = document.getElementById("sideBar");
+     if (x.style.display === "none") {
+         x.style.display = "block";
+     } else {
+         x.style.display = "none";
+     }
+ }
+
+let sidebar = document.getElementById("sidebarleft")
+
+ document.getElementById("sideicon2").addEventListener("click", myFunction);
+ function myFunction() {
+     var x = document.getElementById("sidebarleft");
+     if (x.style.display === "none") {
+         x.style.display = "block";
+     } else {
+         x.style.display = "none";
+     }
+ }
+
+ document.getElementById("sideicon").addEventListener("click", myFunction);
+ function myFunction() {
+     var x = document.getElementById("sidebarleft");
+     if (x.style.display === "none") {
+         x.style.display = "block";
+     } else {
+         x.style.display = "none";
+     }
+ }
